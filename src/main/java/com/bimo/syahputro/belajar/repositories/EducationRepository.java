c00194669659c0485ede1ad835573294f68184a2
bimosyahputro88@gmail.com <bimosyahputro88@gmail.com>
2021-11-11 15:42:06 +0700

    add repository education

---
+++ src/main/java/com/bimo/syahputro/belajar/repositories/EducationRepository.java
@@ -0,0 +1,8 @@
+package com.bimo.syahputro.belajar.repositories;
+
+import com.bimo.syahputro.belajar.entities.EducationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducationRepository extends JpaRepository<EducationEntity,Integer > {
+
+}
