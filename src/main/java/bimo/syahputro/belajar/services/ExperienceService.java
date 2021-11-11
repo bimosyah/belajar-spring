d09ba56d42dff4db7cf7ced28dc40d1428d1b768
bimosyahputro88@gmail.com <bimosyahputro88@gmail.com>
2021-11-11 10:25:54 +0700

    add component

--- src/main/java/bimo/syahputro/belajar/services/ExperienceService.java
+++ src/main/java/bimo/syahputro/belajar/services/ExperienceService.java
@@ -1,9 +1,11 @@
 package bimo.syahputro.belajar.services;
 
 import bimo.syahputro.belajar.entities.ExperienceEntity;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface ExperienceService {
     ExperienceEntity add(ExperienceEntity param);
     ExperienceEntity update(ExperienceEntity param);
