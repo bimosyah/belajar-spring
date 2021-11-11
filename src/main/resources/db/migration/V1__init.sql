27795e623b83864dc9241e5914d35e5bea2a88ca
bimosyahputro88@gmail.com <bimosyahputro88@gmail.com>
2021-11-11 09:46:17 +0700

    init db conf

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,10 +1,56 @@
-CREATE TABLE petani
+CREATE TABLE experience
 (
-    id     int NOT NULL AUTO_INCREMENT,
-    nama   varchar(100),
-    code   varchar(100),
-    alamat varchar(100),
-    noKtp  varchar(100),
-    noTelp varchar(100),
+    id             int NOT NULL AUTO_INCREMENT,
+    title          varchar(100),
+    employmentType varchar(100),
+    companyName    varchar(100),
+    location       varchar(100),
+    startDate      date,
+    endDate        date,
+    industry       varchar(100),
+    description    text,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE education
+(
+    id          int NOT NULL AUTO_INCREMENT,
+    name        varchar(100),
+    degree      varchar(100),
+    major       varchar(100),
+    startDate   date,
+    endDate     date,
+    description text,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE sertificate
+(
+    id             int NOT NULL AUTO_INCREMENT,
+    name           varchar(100),
+    organitation   varchar(100),
+    expirationDate date,
+    credentialId   varchar(200),
+    credentialUrl  text,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE project
+(
+    id          int NOT NULL AUTO_INCREMENT,
+    name        varchar(100),
+    startDate   date,
+    endDate     date,
+    projectUrl  text,
+    description text,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE projectDetail
+(
+    id        int NOT NULL AUTO_INCREMENT,
+    idProject int,
+    imageUrl  text,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
